9e87424b0a17568bf48bb8830a89ac53d3ab3a30
DHannaway <danielhannaway95@gmail.com>
2017-08-07 12:46:04 +0100

    Added full range of parameters to be changed in the lab script

--- input.py
+++ input.py
@@ -6,11 +6,14 @@ import os
 #Path to the executable file:
 codepath='C:\\Users\\Dan\\Desktop\\"Summer_Project"\\testDustSimulation.exe'
 
-#Enter new values for the four parameters
-param1=input("Enter the value of B_over_T (Original value: 0.2): ")	# B_over_T
-param2=input("Enter the value of h_stars (Original value: 0.6 kpc): ")	# h_stars
-param3=input("Enter the value of r_stars (Original value: 5. kpc): ")	# r_stars
-param4=input("Enter the value of h_ISM (Original value: 0.22 kpc): ")	# h_ISM
+#Enter new values for the parameters
+B_T=input("Enter the value of B_over_T (Original value: 0.2): ")	# B_over_T
+h_stars=input("Enter the value of h_stars (Original value: 0.6 kpc): ")	# h_stars
+r_stars=input("Enter the value of r_stars (Original value: 5. kpc): ")	# r_stars
+h_ISM=input("Enter the value of h_ISM (Original value: 0.22 kpc): ")	# h_ISM
+r_ISM=input("Enter the value of r_ISM (Original value: 6.0 kpc): ")	# r_ISM
+n_0=input("Enter the value of n_0 (Original value: 1. cm^-3): ")	# n_0
+
 nphotons=input("Enter the number of photons to simulate (Original value 50,000): ")
 
 #Replace the old .param file with a new one with the entered values
@@ -20,13 +23,17 @@ with open('test_dustsimulation_original.param') as g:
 h=open('test_dustsimulation.param','w')
 for i in range(0,len(lines)):
 	if i==12:
-		h.write('  B_over_T: '+param1+'\n')
+		h.write('  B_over_T: '+B_T+'\n')
 	elif i==13:
-		h.write( '  h_stars: '+param2+' kpc\n' )
+		h.write( '  h_stars: '+h_stars+' kpc\n' )
 	elif i==14:
-		h.write('  r_stars: '+param3+' kpc\n'  )
+		h.write('  r_stars: '+r_stars+' kpc\n'  )
 	elif i==16:
-		h.write('  h_ISM: '+param4+' kpc\n'  )
+		h.write('  h_ISM: '+h_ISM+' kpc\n'  )
+	elif i==18:
+		h.write('  r_ISM: '+r_ISM+' kpc\n'  )
+	elif i==17:
+		h.write('  n_0: '+n_0+' cm^-3\n' )
 	elif i==28:
 		h.write('number of photons: '+nphotons+'\n'  )
 	else:
@@ -41,6 +48,6 @@ image = np.fromfile("test_dustsimulation_output.dat", dtype = np.float64)
 image = image.reshape((200, 200))
 fig, ax = plt.subplots()
 ax.matshow(np.transpose(image), cmap='Greys_r')
-plt.text(5,40,"B_T ="+str(param1)+'\n'+"h_stars ="+str(param2)+'\n'+"r_stars ="+str(param3)+'\n'+"h_ISM ="+str(param4),color='w')
+plt.text(5,40,"B_T ="+str(B_T)+'\n'+"h_stars ="+str(h_stars)+'\n'+"r_stars ="+str(r_stars)+'\n'+"h_ISM ="+str(h_ISM),color='w')
 plt.show()
 
